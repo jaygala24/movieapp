94c51b7844d85ba6a13d5449e7cc9f47b9c2775b
jaygala24 <jaygala24@gmail.com>
2018-12-18 23:13:16 +0530

    Updates

---
+++ precache-manifest.7f320606068cb2fbb9848b995210fdad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d10bba8c01b9f3d022",
+    "url": "/movieapp/static/js/runtime~main.e7d10bba.js"
+  },
+  {
+    "revision": "47e48aa64e8b625f2e51",
+    "url": "/movieapp/static/js/main.47e48aa6.chunk.js"
+  },
+  {
+    "revision": "d25298fb79ffea3a8d3d",
+    "url": "/movieapp/static/js/1.d25298fb.chunk.js"
+  },
+  {
+    "revision": "47e48aa64e8b625f2e51",
+    "url": "/movieapp/static/css/main.ba33dd83.chunk.css"
+  },
+  {
+    "revision": "d25298fb79ffea3a8d3d",
+    "url": "/movieapp/static/css/1.44889043.chunk.css"
+  },
+  {
+    "revision": "86a3466496260ad0d2e76127c87b75ee",
+    "url": "/movieapp/index.html"
+  }
+];
